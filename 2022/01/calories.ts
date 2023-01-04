2ab87fae5ff7a63b3e54bc22db0e01989b04a6bd
Ivan Burda <iburda@Ivans-MacBook-Pro.local>
2023-01-05 00:48:21 +0100

    da1-part1

--- 2022/01/calories.ts
+++ 2022/01/calories.ts
@@ -1,7 +1,33 @@
+import {load} from './load'
 type elfLoad = number[];
 
 
-export const getIndividualElveLoads = (allLoads:string): elfLoad[] => {
+export const getIndividualElveLoads = (allLoadsCrude:string): number[][] => {
+    const result: number[][] = [];
+    let temp: number[] = [];
 
-    return[]
+    for(const element of allLoadsCrude.split('\n')){
+        if(element===''){
+            result.push(temp);
+            temp=[];
+        }
+        else{
+            temp.push(Number(element))
+        }
+    }
+    return result
 };
+
+export const findElfWithHighestLoad = (allLoads:number[][]):number=>{
+    let highestLoadSum = -Infinity;
+
+    for(const array of allLoads){
+        const sum = array.reduce((a,b)=>a+b,0)
+        if(sum>highestLoadSum){
+            highestLoadSum = sum;
+        }
+    }
+    return highestLoadSum;
+}
+
+console.log(findElfWithHighestLoad(getIndividualElveLoads(load)));
