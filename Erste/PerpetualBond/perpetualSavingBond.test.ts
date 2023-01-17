65f969f671ed410b0450456285134bf0beeb4f65
ivanburda1986 <burda.ivan@gmail.com>
2023-01-17 01:28:37 +0100

    test

--- Erste/PerpetualBond/perpetualSavingBond.test.ts
+++ Erste/PerpetualBond/perpetualSavingBond.test.ts
@@ -1,41 +1,62 @@
-import {calculateDaysPerpetualBondsNeedToReachCap, transformRawRecord} from "./perpetualSavingBond";
+import {
+    calculateDaysPerpetualBondsNeedToReachCap,
+    getNumberOfDays,
+    initialIssuedBondsRecord,
+    transformRawRecord,
+} from "./perpetualSavingBond";
+
+describe("getNumberOfDays", () => {
+    it('returns 6 for "Bond PSB#00000001 sold 6 days ago"', () => {
+        expect(getNumberOfDays("Bond PSB#00000001 sold 6 days ago")).toBe("6");
+    });
+});
 
 describe("perpetualSavingBond", () => {
-  it("returns it requires 10 days to reach total bond cap of 100 EUR", () => {
-    expect(calculateDaysPerpetualBondsNeedToReachCap(100)).toBe(10);
-  });
-  it("returns it requires 11 days to reach total bond cap of 130 EUR", () => {
-    expect(calculateDaysPerpetualBondsNeedToReachCap(130)).toBe(11);
-  });
-  it("returns it requires 12 days to reach total bond cap of 140 EUR", () => {
-    expect(calculateDaysPerpetualBondsNeedToReachCap(140)).toBe(12);
-  });
-  it("returns it requires 13 days to reach total bond cap of 150 EUR", () => {
-    expect(calculateDaysPerpetualBondsNeedToReachCap(150)).toBe(13);
-  });
-  it("returns it requires 14 days to reach total bond cap of 160 EUR", () => {
-    expect(calculateDaysPerpetualBondsNeedToReachCap(160)).toBe(14);
-  });
+    it("returns it requires 10 days to reach total bond cap of 100 EUR", () => {
+        expect(
+            calculateDaysPerpetualBondsNeedToReachCap(100, initialIssuedBondsRecord)
+        ).toBe(10);
+    });
+    it("returns it requires 11 days to reach total bond cap of 130 EUR", () => {
+        expect(
+            calculateDaysPerpetualBondsNeedToReachCap(130, initialIssuedBondsRecord)
+        ).toBe(11);
+    });
+    it("returns it requires 12 days to reach total bond cap of 140 EUR", () => {
+        expect(
+            calculateDaysPerpetualBondsNeedToReachCap(140, initialIssuedBondsRecord)
+        ).toBe(12);
+    });
+    it("returns it requires 13 days to reach total bond cap of 150 EUR", () => {
+        expect(
+            calculateDaysPerpetualBondsNeedToReachCap(150, initialIssuedBondsRecord)
+        ).toBe(13);
+    });
+    it("returns it requires 14 days to reach total bond cap of 160 EUR", () => {
+        expect(
+            calculateDaysPerpetualBondsNeedToReachCap(160, initialIssuedBondsRecord)
+        ).toBe(14);
+    });
 });
 
 describe("transformRawRecord", () => {
-  it("returns a perpetual bond count by maturity in the format needed for further processing ", () => {
-    const rawRecord =
-        "Bond PSB#00000001 sold 6 days ago\n" +
-        "Bond PSB#00000008 sold 2 days ago\n" +
-        "Bond PSB#00000003 sold 5 days ago\n" +
-        "Bond PSB#00000002 sold 6 days ago\n" +
-        "Bond PSB#00000004 sold 5 days ago\n" +
-        "Bond PSB#00000006 sold 4 days ago\n" +
-        "Bond PSB#00000007 sold 3 days ago\n" +
-        "Bond PSB#00000005 sold 5 days ago";
+    it("returns a perpetual bond count by maturity in the format needed for further processing ", () => {
+        const rawRecord =
+            "Bond PSB#00000001 sold 6 days ago\n" +
+            "Bond PSB#00000008 sold 2 days ago\n" +
+            "Bond PSB#00000003 sold 5 days ago\n" +
+            "Bond PSB#00000002 sold 6 days ago\n" +
+            "Bond PSB#00000004 sold 5 days ago\n" +
+            "Bond PSB#00000006 sold 4 days ago\n" +
+            "Bond PSB#00000007 sold 3 days ago\n" +
+            "Bond PSB#00000005 sold 5 days ago";
 
-    expect(transformRawRecord(rawRecord)).toEqual([
-      {maturity: 6, bonds: 2},
-      {maturity: 5, bonds: 3},
-      {maturity: 4, bonds: 1},
-      {maturity: 3, bonds: 1},
-      {maturity: 2, bonds: 1},
-    ]);
-  });
+        expect(transformRawRecord(rawRecord)).toEqual([
+            {maturity: 6, bonds: 2},
+            {maturity: 5, bonds: 3},
+            {maturity: 4, bonds: 1},
+            {maturity: 3, bonds: 1},
+            {maturity: 2, bonds: 1},
+        ]);
+    });
 });
--- Erste/PerpetualBond/perpetualSavingBond.ts
+++ Erste/PerpetualBond/perpetualSavingBond.ts
@@ -1,12 +1,12 @@
-const initialIssuedBondsRecord =
-  "Bond PSB#00000001 sold 6 days ago\n" +
-  "Bond PSB#00000008 sold 2 days ago\n" +
-  "Bond PSB#00000003 sold 5 days ago\n" +
-  "Bond PSB#00000002 sold 6 days ago\n" +
-  "Bond PSB#00000004 sold 5 days ago\n" +
-  "Bond PSB#00000006 sold 4 days ago\n" +
-  "Bond PSB#00000007 sold 3 days ago\n" +
-  "Bond PSB#00000005 sold 5 days ago";
+export const initialIssuedBondsRecord =
+    "Bond PSB#00000001 sold 6 days ago\n" +
+    "Bond PSB#00000008 sold 2 days ago\n" +
+    "Bond PSB#00000003 sold 5 days ago\n" +
+    "Bond PSB#00000002 sold 6 days ago\n" +
+    "Bond PSB#00000004 sold 5 days ago\n" +
+    "Bond PSB#00000006 sold 4 days ago\n" +
+    "Bond PSB#00000007 sold 3 days ago\n" +
+    "Bond PSB#00000005 sold 5 days ago";
 
 export const PERPETUAL_BOND_VALUE_EUR = 2;
 export const TRANSACTION_PROCESSING_TIME_DAYS = 3;
@@ -14,94 +14,112 @@ export const DAYS_TO_YIELD_NEW_PERPETUAL_BOND = 7;
 export const ERSTE_TOTAL_ASSETS_EUR = 272000000000;
 
 export interface MaturityEntry {
-  maturity: number;
-  bonds: number;
+    maturity: number;
+    bonds: number;
 }
 
-function getNumberOfDays(rawRecordLine: string) {
-  const days = rawRecordLine.match(/\d+/g);
-  if (days) {
-    return parseInt(days[1]);
-  }
-  return;
+export function getNumberOfDays(rawRecordLine: string) {
+    const days = rawRecordLine.match(/\d+/g);
+    if (days) {
+        return parseInt(days[1]);
+    }
+    return;
 }
 
 export const transformRawRecord = (rawRecord: string) => {
-  const bondCountByMaturity: Record<number, number> = {};
-  const result: MaturityEntry[] = [];
-  for (const recordLine of rawRecord.split("\n")) {
-    const recordLineMaturity: number | undefined = getNumberOfDays(recordLine);
-    if (recordLineMaturity && bondCountByMaturity[recordLineMaturity]) {
-      bondCountByMaturity[recordLineMaturity] += 1;
-    } else if (recordLineMaturity) {
-      bondCountByMaturity[recordLineMaturity] = 1;
+    const bondCountByMaturity: Record<number, number> = {};
+    const result: MaturityEntry[] = [];
+    for (const recordLine of rawRecord.split("\n")) {
+        const recordLineMaturity: number | undefined = getNumberOfDays(recordLine);
+        if (recordLineMaturity && bondCountByMaturity[recordLineMaturity]) {
+            bondCountByMaturity[recordLineMaturity] += 1;
+        } else if (recordLineMaturity) {
+            bondCountByMaturity[recordLineMaturity] = 1;
+        }
     }
-  }
 
-  for (const key in bondCountByMaturity) {
-    result.push({ maturity: parseInt(key), bonds: bondCountByMaturity[key] });
-  }
+    for (const key in bondCountByMaturity) {
+        result.push({maturity: parseInt(key), bonds: bondCountByMaturity[key]});
+    }
 
-  return result.sort((a, b) => b.maturity - a.maturity);
+    return result.sort((a, b) => b.maturity - a.maturity);
 };
 
 export const areBondsMatureToYieldNewBond = (bondsByMaturity: MaturityEntry) =>
-  bondsByMaturity.maturity - TRANSACTION_PROCESSING_TIME_DAYS > 0 &&
-  (bondsByMaturity.maturity - TRANSACTION_PROCESSING_TIME_DAYS) %
+    bondsByMaturity.maturity - TRANSACTION_PROCESSING_TIME_DAYS > 0 &&
+    (bondsByMaturity.maturity - TRANSACTION_PROCESSING_TIME_DAYS) %
     DAYS_TO_YIELD_NEW_PERPETUAL_BOND ===
     0;
 
 export const isRequiredCapitalizationReached = (
-  issuedPerpetualBondCount: number,
-  issuedPerpetualBondTotalCapTarget: number
+    issuedPerpetualBondCount: number,
+    issuedPerpetualBondTotalCapTarget: number
 ) =>
-  issuedPerpetualBondCount * PERPETUAL_BOND_VALUE_EUR >
-  issuedPerpetualBondTotalCapTarget;
+    issuedPerpetualBondCount * PERPETUAL_BOND_VALUE_EUR >
+    issuedPerpetualBondTotalCapTarget;
 
-export const calculateDaysPerpetualBondsNeedToReachCap = (
-  issuedPerpetualBondTotalCapTarget: number
-): number => {
-  const issuedPerpetualBondCountByMaturity: MaturityEntry[] =
-    transformRawRecord(initialIssuedBondsRecord);
-  let perpetualBondProgramDayCount: number =
-    issuedPerpetualBondCountByMaturity[0].maturity;
-  let issuedPerpetualBondCount: number =
-    issuedPerpetualBondCountByMaturity.reduce((a, b) => a.bonds + b.bonds);
+function sumBonds(issuedPerpetualBondCountByMaturity: MaturityEntry[]) {
+    return issuedPerpetualBondCountByMaturity.reduce(
+        (acc, cur) => acc + cur.bonds,
+        0
+    );
+}
 
-  function executeNewDayPerpetualBondJob(): void {
-    perpetualBondProgramDayCount++;
+function executeNewDayPerpetualBondJob(
+    issuedPerpetualBondCountByMaturity: MaturityEntry[],
+    perpetualBondProgramDayCount: number
+): {
+    dayCount: number;
+    bondCountByMaturity: MaturityEntry[];
+} {
     let newlyIssuedPerpetualBondCount = 0;
 
     issuedPerpetualBondCountByMaturity.forEach((bondsByMaturity) => {
-      bondsByMaturity.maturity++;
-      if (areBondsMatureToYieldNewBond(bondsByMaturity)) {
-        newlyIssuedPerpetualBondCount += bondsByMaturity.bonds;
-      }
+        bondsByMaturity.maturity++;
+        if (areBondsMatureToYieldNewBond(bondsByMaturity)) {
+            newlyIssuedPerpetualBondCount += bondsByMaturity.bonds;
+        }
     });
 
     if (newlyIssuedPerpetualBondCount) {
-      issuedPerpetualBondCountByMaturity.push({
-        maturity: 1,
-        bonds: newlyIssuedPerpetualBondCount,
-      });
-      issuedPerpetualBondCount += newlyIssuedPerpetualBondCount;
+        issuedPerpetualBondCountByMaturity.push({
+            maturity: 1,
+            bonds: newlyIssuedPerpetualBondCount,
+        });
     }
     return {
-      perpetualBondProgramDayCount: perpetualBondProgramDayCount + 1,
+        dayCount: perpetualBondProgramDayCount + 1,
+        bondCountByMaturity: issuedPerpetualBondCountByMaturity,
     };
-  }
+}
 
-  while (
-    !isRequiredCapitalizationReached(
-      issuedPerpetualBondCount,
-      issuedPerpetualBondTotalCapTarget
-    )
-  ) {
-    executeNewDayPerpetualBondJob();
-  }
+export const calculateDaysPerpetualBondsNeedToReachCap = (
+    issuedPerpetualBondTotalCapTarget: number,
+    initialIssuedBondsRecord: string
+): number => {
+    let issuedPerpetualBondCountByMaturity: MaturityEntry[] = transformRawRecord(initialIssuedBondsRecord);
+    let perpetualBondProgramDayCount: number = 6;
+    let issuedPerpetualBondCount: number = 8;
 
-  console.log(perpetualBondProgramDayCount);
-  return perpetualBondProgramDayCount;
+    while (
+        !isRequiredCapitalizationReached(
+            issuedPerpetualBondCount,
+            issuedPerpetualBondTotalCapTarget
+        )
+        ) {
+        const {dayCount, bondCountByMaturity} = executeNewDayPerpetualBondJob(
+            issuedPerpetualBondCountByMaturity,
+            perpetualBondProgramDayCount
+        );
+        perpetualBondProgramDayCount = dayCount;
+        issuedPerpetualBondCountByMaturity = bondCountByMaturity;
+    }
+
+    console.log(perpetualBondProgramDayCount);
+    return perpetualBondProgramDayCount;
 };
 
-calculateDaysPerpetualBondsNeedToReachCap(ERSTE_TOTAL_ASSETS_EUR);
+calculateDaysPerpetualBondsNeedToReachCap(
+    ERSTE_TOTAL_ASSETS_EUR,
+    initialIssuedBondsRecord
+);
