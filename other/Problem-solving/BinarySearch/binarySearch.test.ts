92b809817ffaf544beebd6b0c52a93e8615adc12
ivanburda1986 <burda.ivan@gmail.com>
2023-04-16 23:23:28 +0200

    added solution from the course

--- other/Problem-solving/BinarySearch/binarySearch.test.ts
+++ other/Problem-solving/BinarySearch/binarySearch.test.ts
@@ -1,32 +1,32 @@
-import {binarySearch} from "./binarySearch";
+import {binarySearchIvanBurda} from "./binarySearch";
 
-describe('binarySearch', () => {
+describe('binarySearchIvanBurda', () => {
     it('returns', () => {
-        expect(binarySearch([1, 2, 3, 4, 5], 2)).toBe(1);
+        expect(binarySearchIvanBurda([1, 2, 3, 4, 5], 2)).toBe(1);
     });
     it('returns', () => {
-        expect(binarySearch([1, 2, 3, 4, 5], 3)).toBe(2);
+        expect(binarySearchIvanBurda([1, 2, 3, 4, 5], 3)).toBe(2);
     });
     it('returns', () => {
-        expect(binarySearch([1, 2, 3, 4, 5], 5)).toBe(4);
+        expect(binarySearchIvanBurda([1, 2, 3, 4, 5], 5)).toBe(4);
     });
     it('returns', () => {
-        expect(binarySearch([1, 2, 3, 4, 5], 6)).toBe(-1);
+        expect(binarySearchIvanBurda([1, 2, 3, 4, 5], 6)).toBe(-1);
     });
     it('returns', () => {
-        expect(binarySearch([
+        expect(binarySearchIvanBurda([
             5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
             40, 44, 64, 79, 84, 86, 95, 96, 98, 99
         ], 10)).toBe(2);
     });
     it('returns', () => {
-        expect(binarySearch([
+        expect(binarySearchIvanBurda([
             5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
             40, 44, 64, 79, 84, 86, 95, 96, 98, 99
         ], 95)).toBe(16);
     });
     it('returns', () => {
-        expect(binarySearch([
+        expect(binarySearchIvanBurda([
             5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
             40, 44, 64, 79, 84, 86, 95, 96, 98, 99
         ], 100)).toBe(-1);
--- other/Problem-solving/BinarySearch/binarySearch.ts
+++ other/Problem-solving/BinarySearch/binarySearch.ts
@@ -1,4 +1,4 @@
-export function binarySearch(sortedArr: number[], value: number) {
+export function binarySearchIvanBurda(sortedArr: number[], value: number) {
     let pointer1 = 0;
     let pointer2 = sortedArr.length - 1;
 
@@ -23,6 +23,27 @@ export function binarySearch(sortedArr: number[], value: number) {
             middle = Math.floor((pointer2 - pointer1) / 2);
 
         }
+    }
+    return -1;
+}
+
+export function binarySearchColdSteel(arr: number[], elem: number) {
+    let start = 0;
+    let end = arr.length - 1;
+
+    let middle = Math.floor((start + end) / 2);
+
+    while (arr[middle] !== elem && start <= end) {
+        if (elem < arr[middle]) {
+            end = middle - 1;
+
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+    if (arr[middle] === elem) {
+        return middle;
     }
     return -1;
 }
