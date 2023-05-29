c35fc0d9d838f65f2b2512138d7658cbebae884e
ivanburda1986 <burda.ivan@gmail.com>
2023-05-30 01:44:17 +0200

    day 3 2021

--- 2021/03/2021-03.ts
+++ 2021/03/2021-03.ts
@@ -46,3 +46,49 @@ export const getPowerConsumption = (input: string) => {
 
     return (parseInt(gamaRate, 2) * parseInt(epsilonRate, 2));
 };
+/*
+go through lines 1 by one
+each time divide the lines by those with more 0 and those with more 1 bits
+go
+ */
+
+
+export const getDividedBinaries = (input: string[], position: number): { dominant: string[], weak: string[] } => {
+    const withZero: string[] = [];
+    const withOne: string[] = [];
+    input.forEach((binary) => binary[position] === "1" ? withOne.push(binary) : withZero.push(binary));
+    if (withZero.length > withOne.length) {
+        return {dominant: withZero, weak: withOne};
+    }
+    return {dominant: withOne, weak: withZero};
+};
+
+export const getRatings = (input: string) => {
+    let oxygenBinaries = input.split(`\n`);
+    let carbonDioxideBinaries = input.split(`\n`);
+    let oxygenPosition = 0;
+    let carbonPosition = 0;
+    let oxygenRating = 0;
+    let carbonRating = 0;
+    while (oxygenBinaries.length > 2) {
+        oxygenBinaries = getDividedBinaries(oxygenBinaries, oxygenPosition).dominant;
+        oxygenPosition++;
+    }
+    while (carbonDioxideBinaries.length > 2) {
+        carbonDioxideBinaries = getDividedBinaries(carbonDioxideBinaries, carbonPosition).weak;
+        carbonPosition++;
+    }
+    if (oxygenBinaries[0].slice(-1) === "1") {
+        oxygenRating = parseInt(oxygenBinaries[0], 2);
+    } else {
+        oxygenRating = parseInt(oxygenBinaries[1], 2);
+    }
+
+    if (carbonDioxideBinaries[0].slice(-1) === "0") {
+        carbonRating = parseInt(carbonDioxideBinaries[0], 2);
+    } else {
+        carbonRating = parseInt(carbonDioxideBinaries[1], 2);
+    }
+    return oxygenRating * carbonRating;
+};
+
--- 2021/03/__tests__/2021-03.test.ts
+++ 2021/03/__tests__/2021-03.test.ts
@@ -1,4 +1,4 @@
-import {getPowerConsumption} from "../2021-03";
+import {getPowerConsumption, getRatings} from "../2021-03";
 import {readFileSync} from "fs";
 
 describe('', () => {
@@ -23,6 +23,29 @@ describe('', () => {
         it('returns 3687446 power consumption for the real input', () => {
             const input = readFileSync('2021/03/2021-03-1.txt', {encoding: 'utf8', flag: 'r'});
             expect(getPowerConsumption(input)).toBe(3687446);
+        });
+    });
+
+    describe('getLifeSupportRating', () => {
+        it('returns 230 for the sample input', () => {
+            const input = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
+
+            expect(getRatings(input)).toBe(230);
+        });
+        it('returns 4406844 for the real input', () => {
+            const input = readFileSync('2021/03/2021-03-1.txt', {encoding: 'utf8', flag: 'r'});
+            expect(getRatings(input)).toBe(4406844);
         });
     });
 });
