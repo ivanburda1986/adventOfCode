1760800a953ed7baa743df077ac14855e782a57a
ivanburda1986 <burda.ivan@gmail.com>
2023-06-11 00:09:55 +0200

    2021-d5-p1 finished

--- 2021/05/2021-05.ts
+++ 2021/05/2021-05.ts
@@ -1,3 +1,162 @@
+import {defaultDict} from "../../utils/defaultDictionary";
+/*
+Split input by line breaks
+For each line, get: [[8,0],[0,8]]
+
+Evaluate where the line is horizontal or vertical
+x === x || y === 0
+horizontal: number for x (i.e. the first number) is the same for both value
+vertical: number for y (i.e. the second number) is the same for both values
+
+For horizontal and vertical:
+ * Map where key is line X number
+ * and value is a map where key is Y and value number of vents at the position
+ * For 9,4 -> 3,4 ====>
+ * represent the range as: [[9,4],[8,4],[7,4], [5,4],[4,4],[3,4]]
+ * go over X and access the map at the X position and then access the nested map at the Y position and mark it as vent
+ * if going over Y and it is already marked as vent, increase the overall count of fields where at least 2 vents are located
+
+ const ventMap = {
+ "9":{
+      "4":1},
+ "8":{
+      "4":1},
+ "7":{
+      "4":1},
+ "6":{
+      "4":1},
+ "5":{
+      "4":1},
+ "4":{
+      "4":1},
+ "3":{
+      "4":1},
+
+ }
+
+For diagonal
+ * For 8,0 -> 0,8 ====> [[8,0],[7,1],[6,2],[5,3],[4,4],[3,5],[2,6],[1,7],[0,8]]
+ *
+ *
+ */
+type LineCoordinates = [string, string, string, string]
+type VentCoordinates = [string, string];
+export const getParsedInput = (input: string): LineCoordinates[] => {
+    const lines = input.split("\n");
+    return lines.map((line) => {
+        const [, ax, ay, bx, by] = line.match(/(\d+),(\d+) -> (\d+),(\d+)/) || [];
+        return [ax, ay, bx, by];
+    });
+};
+
+interface SortedLinesByDirection {
+    horizontal: LineCoordinates[],
+    vertical: LineCoordinates[],
+    diagonal: LineCoordinates[],
+}
+
+export const sortLinesByDirection = (lineCoordinates: LineCoordinates[]): SortedLinesByDirection => {
+    const horizontal: LineCoordinates[] = [];
+    const vertical: LineCoordinates[] = [];
+    const diagonal: LineCoordinates[] = [];
+    lineCoordinates.forEach(line => {
+        if (line[0] === line[2] || line[1] === line[3]) {
+            if (line[0] === line[2]) {
+                vertical.push(line);
+                return;
+            }
+            if (line[1] === line[3]) {
+                horizontal.push(line);
+                return;
+            }
+        }
+        if (line[0] === line[2] || line[1] === line[3]) {
+            horizontal.push(line);
+            return;
+        }
+
+        diagonal.push(line);
+    });
+
+    return {horizontal, vertical, diagonal};
+};
+
+export enum LineDirection {
+    horizontal = "horizontal",
+    vertical = "vertical"
+}
+
+export const getLineFullCoordinates = (coordinates: LineCoordinates, lineDirection: LineDirection): VentCoordinates[] => {
+    const numericalCoordinates = coordinates.map(coordinate => Number(coordinate));
+    const [x1, y1, x2, y2] = coordinates.map(coordinate => Number(coordinate));
+    const numericalCoordinatesRange = [];
+
+    // ["0", "9", "5", "9"]
+    if (lineDirection === LineDirection.horizontal) {
+        if (x1 < x2) {
+            for (let i = x1; i <= x2; i++) {
+                numericalCoordinatesRange.push([i, y1]);
+            }
+        }
+        if (x1 > x2) {
+            for (let i = x2; i <= x1; i++) {
+                numericalCoordinatesRange.push([i, y1]);
+            }
+        }
+    }
+    // ["7", "0", "7", "4"]
+    if (lineDirection === LineDirection.vertical) {
+        if (y1 < y2) {
+            for (let i = y1; i <= y2; i++) {
+                numericalCoordinatesRange.push([x1, i]);
+            }
+        }
+        if (y1 > y2) {
+            for (let i = y2; i <= y1; i++) {
+                numericalCoordinatesRange.push([x1, i]);
+            }
+        }
+    }
+    return numericalCoordinatesRange.map(([x, y]) => [x.toString(), y.toString()]);
+};
+
+export class VentMap {
+    public ventMap: Record<string, Record<string, number>> = {};
+
+    addCoordinates(coordinates: VentCoordinates) {
+        const [x, y] = coordinates;
+        if (x in this.ventMap) {
+            if (y in this.ventMap[x]) {
+                this.ventMap[x][y]++;
+            } else {
+                this.ventMap[x][y] = 1;
+            }
+        } else {
+            this.ventMap[x] = {[y]: 1};
+        }
+    }
+
+    getCoordinatesCount(coordinates: VentCoordinates) {
+        const [x, y] = coordinates;
+        return this.ventMap[x][y];
+    }
+
+    getOverlapCount() {
+        let overlapCount = 0;
+        Object.values(this.ventMap).forEach(xCombination => Object.values(xCombination).forEach(xyCoordinates => xyCoordinates > 1 ? overlapCount++ : null));
+        return overlapCount;
+    }
+
+}
+
 export const getOverlapCount = (input: string): number => {
-    return 1;
+    const myVentMap = new VentMap();
+    const linesSortedByDirection: SortedLinesByDirection = sortLinesByDirection(getParsedInput(input));
+    const horizontalLinesAllCoordinates: VentCoordinates[][] = [];
+    const verticalLinesAllCoordinates: VentCoordinates[][] = [];
+    linesSortedByDirection.horizontal.forEach(line => horizontalLinesAllCoordinates.push(getLineFullCoordinates(line, LineDirection.horizontal)));
+    linesSortedByDirection.vertical.forEach(line => verticalLinesAllCoordinates.push(getLineFullCoordinates(line, LineDirection.vertical)));
+    horizontalLinesAllCoordinates.forEach(oneLineAllCoordinates => oneLineAllCoordinates.forEach(singleCoordinatesOfLine => myVentMap.addCoordinates(singleCoordinatesOfLine)));
+    verticalLinesAllCoordinates.forEach(oneLineAllCoordinates => oneLineAllCoordinates.forEach(singleCoordinatesOfLine => myVentMap.addCoordinates(singleCoordinatesOfLine)));
+    return myVentMap.getOverlapCount();
 };
--- 2021/05/__tests__/2021-05.test.ts
+++ 2021/05/__tests__/2021-05.test.ts
@@ -1,9 +1,7 @@
-import {getOverlapCount} from "../2021-05";
+import {getLineFullCoordinates, getOverlapCount, getParsedInput, LineDirection, sortLinesByDirection} from "../2021-05";
 import {readFileSync} from "fs";
 
-describe('AOC-2021-05', () => {
-    it('returns 5 as the number of points where at least two coordinate lines overlap in the sample data', () => {
-        const input = `0,9 -> 5,9
+const sampleInput = `0,9 -> 5,9
 8,0 -> 0,8
 9,4 -> 3,4
 2,2 -> 2,1
@@ -13,34 +11,59 @@ describe('AOC-2021-05', () => {
 3,4 -> 1,4
 0,0 -> 8,8
 5,5 -> 8,2`;
-        expect(getOverlapCount(input)).toBe(5);
+
+
+describe('AOC-2021-05', () => {
+    describe('getLineFullCoordinates', () => {
+        it('returns full coordinates for line1', () => {
+            expect(getLineFullCoordinates(["0", "9", "5", "9"], LineDirection.horizontal)).toEqual([["0", "9"], ["1", "9"], ["2", "9"], ["3", "9"], ["4", "9"], ["5", "9"]]);
+        });
+
+        // it('returns full coordinates for line2', () => {
+        //     expect(getLineFullCoordinates(["8", "0", "0", "8"])).toEqual([]);
+        // });
+
+        it('returns full coordinates for line3', () => {
+            expect(getLineFullCoordinates(["9", "4", "3", "4"], LineDirection.horizontal)).toEqual([["3", "4"], ["4", "4"], ["5", "4"], ["6", "4"], ["7", "4"], ["8", "4"], ["9", "4"]]);
+        });
+
+        it('returns full coordinates for line4', () => {
+            expect(getLineFullCoordinates(["2", "2", "2", "1"], LineDirection.vertical)).toEqual([["2", "1"], ["2", "2"]]);
+        });
+
+        it('returns full coordinates for line5', () => {
+            expect(getLineFullCoordinates(["7", "0", "7", "4"], LineDirection.vertical)).toEqual([["7", "0"], ["7", "1"], ["7", "2"], ["7", "3"], ["7", "4"]]);
+        });
     });
 
-    it('returns 123 as the number of points where at least two coordinate lines overlap in the real data', () => {
-        const input = readFileSync('2021/05/2021-05.txt', {encoding: 'utf8', flag: 'r'});
-        expect(getOverlapCount(input)).toBe(5);
+    describe('sortLinesByDirection', () => {
+        it('returns coordinates sorted by direction', () => {
+            expect(sortLinesByDirection(getParsedInput(sampleInput))).toEqual({
+                "diagonal": [["8", "0", "0", "8"], ["6", "4", "2", "0"], ["0", "0", "8", "8"], ["5", "5", "8", "2"]],
+                "horizontal": [["0", "9", "5", "9"], ["9", "4", "3", "4"], ["0", "9", "2", "9"], ["3", "4", "1", "4"]],
+                "vertical": [["2", "2", "2", "1"], ["7", "0", "7", "4"]]
+            });
+        });
+    });
+
+    describe('getOverlapCount', () => {
+        it('returns 5 as the number of points where at least two horizontal/vertical lines overlap in the sample data', () => {
+            expect(getOverlapCount(sampleInput)).toBe(5);
+        });
+
+        it('returns 5280 as the number of points where at least two horizontal/vertical lines overlap in the real data', () => {
+            const input = readFileSync('2021/05/2021-05.txt', {encoding: 'utf8', flag: 'r'});
+            expect(getOverlapCount(input)).toBe(5280);
+        });
+
+        it('returns 12 as the number of points where at least two horizontal/vertical/diagonal lines overlap in the sample data', () => {
+            expect(getOverlapCount(sampleInput)).toBe(5);
+        });
+
+        it('returns 12 as the number of points where at least two horizontal/vertical/diagonal lines overlap in the real data', () => {
+            const input = readFileSync('2021/05/2021-05.txt', {encoding: 'utf8', flag: 'r'});
+            expect(getOverlapCount(input)).toBe(5);
+        });
     });
 });
 
-/*
-Split input by line breaks
-For each line, get: [[8,0],[0,8]]
-
-Evaluate where the line is horizontal or vertical
-x === x || y === 0
-horizontal: number for x (i.e. the first number) is the same for both value
-vertical: number for y (i.e. the second number) is the same for both values
-
-For horizontal and vertical:
- * Map where key is line X number
- * and value is a map where key is Y and value number of vents at the position
- * For 9,4 -> 3,4 ====>
- * represent the range as: [[9,4],[8,4],[7,4], [5,4],[4,4],[3,4]]
- * go over X and access the map at the X position and then access the nested map at the Y position and mark it as vent
- * if going over Y and it is already marked as vent, increase the overall count of fields where at least 2 vents are located
-
-For diagonal
- * For 8,0 -> 0,8 ====> [[8,0],[7,1],[6,2],[5,3],[4,4],[3,5],[2,6],[1,7],[0,8]]
- *
- *
- */
