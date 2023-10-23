c3030990c56e7fc8a1d6e264a63c9ea01c06d82a
ivanburda1986 <burda.ivan@gmail.com>
2023-10-23 13:56:53 +0200

    2020-3

--- 2020/03/2020-03.ts
+++ 2020/03/2020-03.ts
@@ -1,3 +1,19 @@
-describe('', () => {
-    
-});
+export const getTrees = (input: string, dx: number = 3, dy: number = 1): number => {
+    const slopLines = input.split('\n').map(line => line.split(''));
+    let treesHit = 0;
+
+    for (let x = 0, y = 0; y < slopLines.length; y = y + dy, x = x + dx) {
+        if (x >= slopLines[y].length) {
+            x = x - slopLines[y].length;
+        }
+        if (slopLines[y][x] === "#") {
+            treesHit++;
+        }
+    }
+
+    return treesHit;
+};
+
+export const getTreesMultiplication = (input: string): number => [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    .map(([x, y]) => getTrees(input, x, y))
+    .reduce((t, result) => t * result, 1);
--- 2020/03/__tests__/2020-03.test.ts
+++ 2020/03/__tests__/2020-03.test.ts
@@ -1,3 +1,44 @@
-describe('AOC-2020-03', () => {
+import {getTrees, getTreesMultiplication} from "../2020-03";
+import {readFileSync} from "fs";
 
+describe('AOC-2020-03', () => {
+    it('returns 7 as a number of trees for the sample input', () => {
+        const input = `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`;
+        expect(getTrees(input)).toBe(7);
+    });
+
+    it('returns 205 as a number of tress for the real input', () => {
+        const input = readFileSync('2020/03/2020-03.txt', {encoding: 'utf8', flag: 'r'});
+        expect(getTrees(input)).toBe(205);
+    });
+
+    it('returns 336 as multiplication of trees for all paths in the sample input', () => {
+        const input = `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`;
+        expect(getTreesMultiplication(input)).toBe(336);
+    });
+
+    it('returns 3952146825 as multiplication of trees for all paths in the real input', () => {
+        const input = readFileSync('2020/03/2020-03.txt', {encoding: 'utf8', flag: 'r'});
+        expect(getTreesMultiplication(input)).toBe(3952146825);
+    });
 });
