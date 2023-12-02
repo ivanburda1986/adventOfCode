5b37cadaa2f9cc7e03c584431783c72e7503431e
ivanburda1986 <burda.ivan@gmail.com>
2023-12-02 09:36:26 +0100

    23-2-1

--- 2023/02/2023-02.ts
+++ 2023/02/2023-02.ts
@@ -1,17 +1,22 @@
 export const sumImpossibleGameIds = (i: string) => {
-    const result = i.split('Game ')
+    return i.split('Game ')
         .map(game => game.substring(game.indexOf(":") + 2))
         .map(game => game.replaceAll('\n', ""))
         .filter(Boolean)
-        .map(game => game.split('\; '))
-        .map(game => game
-            .map(game => game.split(', '))
-            .map(handGrab => handGrab
-                .map(handGrab => ({
-                    color: handGrab.substring(handGrab.indexOf(" ")).trim(),
-                    count: Number(handGrab.substring(0, handGrab.indexOf("")))
-                })).filter(handGrab => handGrab.color))
-        );
+        .map(game => game.replaceAll(';', ","))
+        .map(game => game.split(', '))
+        .map(game => game.filter(game => {
+            const color = game.substring(game.indexOf(" ")).trim();
 
-    console.log(result);
+            const count = parseInt(game.replace(/\D/g, ''), 10);
+
+            if (color === 'red' && count > 12) {
+                return true;
+            }
+            if (color === 'green' && count > 13) {
+                return true;
+            }
+            return color === 'blue' && count > 14;
+        }))
+        .reduce((accumulated, current, currentIndex) => !current.length ? accumulated + currentIndex + 1 : accumulated, 0);
 };
