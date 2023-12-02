370373575674fa493610aa14c5ab886cb4e2b340
ivanburda1986 <burda.ivan@gmail.com>
2023-12-02 10:21:07 +0100

    simplify

--- 2023/02/2023-02.ts
+++ 2023/02/2023-02.ts
@@ -1,13 +1,17 @@
-export const sumImpossibleGameIds = (i: string) => {
+function getGames(i: string) {
     return i.split('Game ')
         .map(game => game.substring(game.indexOf(":") + 2))
         .map(game => game.replaceAll('\n', ""))
         .filter(Boolean)
         .map(game => game.replaceAll(';', ","))
-        .map(game => game.split(', '))
+        .map(game => game.split(', '));
+}
+
+
+export const sumImpossibleGameIds = (i: string) => {
+    return getGames(i)
         .map(game => game.filter(game => {
             const color = game.substring(game.indexOf(" ")).trim();
-
             const count = parseInt(game.replace(/\D/g, ''), 10);
 
             if (color === 'red' && count > 12) {
@@ -21,30 +25,25 @@ export const sumImpossibleGameIds = (i: string) => {
         .reduce((accumulated, current, currentIndex) => !current.length ? accumulated + currentIndex + 1 : accumulated, 0);
 };
 
-export const powerOfMinCubeCounts = (i: string) => {
-    const games = i.split('Game ')
-        .map(game => game.substring(game.indexOf(":") + 2))
-        .map(game => game.replaceAll('\n', ""))
-        .filter(Boolean)
-        .map(game => game.replaceAll(';', ","))
-        .map(game => game.split(', '))
-    ;
-    const gamesHighestColorCounts = games.map(game => {
-        const colorCounts = new Map();
-        game.forEach(pull => {
-            const color = pull.substring(pull.indexOf(" ")).trim();
-            const count = parseInt(pull.replace(/\D/g, ''), 10);
-            if (!colorCounts.has(color)) {
-                colorCounts.set(color, count);
-            }
-            if (colorCounts.has(color) && colorCounts.get(color) < count) {
-                colorCounts.set(color, count);
-            }
-        });
-        return colorCounts;
 
-    });
-    return gamesHighestColorCounts.map(gameHighestCounts =>
-        gameHighestCounts.get('red') * gameHighestCounts.get('green') * gameHighestCounts.get('blue')
-    ).reduce((accumulated, current) => accumulated + current, 0);
+export const powerOfMinCubeCounts = (i: string) => {
+    return getGames(i)
+        .map(game => {
+            const colorCounts = new Map();
+            game.forEach(pull => {
+                const color = pull.substring(pull.indexOf(" ")).trim();
+                const count = parseInt(pull.replace(/\D/g, ''), 10);
+                if (!colorCounts.has(color)) {
+                    colorCounts.set(color, count);
+                }
+                if (colorCounts.get(color) < count) {
+                    colorCounts.set(color, count);
+                }
+            });
+            return colorCounts;
+        })
+        .map(gameHighestCounts =>
+            gameHighestCounts.get('red') * gameHighestCounts.get('green') * gameHighestCounts.get('blue')
+        )
+        .reduce((accumulated, current) => accumulated + current, 0);
 };
