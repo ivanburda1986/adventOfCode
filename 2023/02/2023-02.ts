86a25fc3d2d641d642ece05219731ff182a0df00
ivanburda1986 <burda.ivan@gmail.com>
2023-12-02 10:10:07 +0100

    23-2-2

--- 2023/02/2023-02.ts
+++ 2023/02/2023-02.ts
@@ -20,3 +20,31 @@ export const sumImpossibleGameIds = (i: string) => {
         }))
         .reduce((accumulated, current, currentIndex) => !current.length ? accumulated + currentIndex + 1 : accumulated, 0);
 };
+
+export const powerOfMinCubeCounts = (i: string) => {
+    const games = i.split('Game ')
+        .map(game => game.substring(game.indexOf(":") + 2))
+        .map(game => game.replaceAll('\n', ""))
+        .filter(Boolean)
+        .map(game => game.replaceAll(';', ","))
+        .map(game => game.split(', '))
+    ;
+    const gamesHighestColorCounts = games.map(game => {
+        const colorCounts = new Map();
+        game.forEach(pull => {
+            const color = pull.substring(pull.indexOf(" ")).trim();
+            const count = parseInt(pull.replace(/\D/g, ''), 10);
+            if (!colorCounts.has(color)) {
+                colorCounts.set(color, count);
+            }
+            if (colorCounts.has(color) && colorCounts.get(color) < count) {
+                colorCounts.set(color, count);
+            }
+        });
+        return colorCounts;
+
+    });
+    return gamesHighestColorCounts.map(gameHighestCounts =>
+        gameHighestCounts.get('red') * gameHighestCounts.get('green') * gameHighestCounts.get('blue')
+    ).reduce((accumalated, current) => accumalated + current, 0);
+};
--- 2023/02/__tests__/2023-02.test.ts
+++ 2023/02/__tests__/2023-02.test.ts
@@ -1,4 +1,4 @@
-import {sumImpossibleGameIds} from "../2023-02";
+import {powerOfMinCubeCounts, sumImpossibleGameIds} from "../2023-02";
 import {readFileSync} from "fs";
 
 describe('AOC-2023-02', () => {
@@ -14,5 +14,19 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
     it('part1 returns 2369 for real input', () => {
         const input = readFileSync('2023/02/2023-02.txt', {encoding: 'utf8', flag: 'r'});
         expect(sumImpossibleGameIds(input)).toBe(2369);
+    });
+
+    it('part2 returns 2286 for test input', () => {
+        const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+        expect(powerOfMinCubeCounts(input)).toBe(2286);
+    });
+
+    it('part1 returns 66363 for real input', () => {
+        const input = readFileSync('2023/02/2023-02.txt', {encoding: 'utf8', flag: 'r'});
+        expect(powerOfMinCubeCounts(input)).toBe(66363);
     });
 });
