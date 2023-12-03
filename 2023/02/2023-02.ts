c452d5eb537c7e08fb707eab2119dff48fd0b0bd
ivanburda1986 <burda.ivan@gmail.com>
2023-12-03 22:03:42 +0100

    wip

--- 2023/02/2023-02.ts
+++ 2023/02/2023-02.ts
@@ -17,7 +17,7 @@ export const getSumOfImpossibleGameIds = (gamesInput: string) =>
                 if (color === 'green' && count > 13) {
                     return true;
                 }
-                return color === 'blue' && count > 14;
+                return count > 14;
             }))
         .reduce((accumulated, current, currentIndex) => !current.length ? accumulated + currentIndex + 1 : accumulated, 0);
 
--- 2023/03/__tests__/2023-03.test.ts
+++ 2023/03/__tests__/2023-03.test.ts
@@ -1,1 +1,28 @@
-describe('AOC-2023-03', () => {});
+import {readFileSync} from "fs";
+import {getPartSum} from "../2023-03";
+
+describe('AOC-2023-03', () => {
+    it('part1 returns 4361 as a part sum for test input', () => {
+        const input = `..........
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+..........`;
+        expect(getPartSum(input)).toBe(4361);
+    });
+
+    it('part1 returns xxx as a part sum for real input', () => {
+        const input = readFileSync('2023/03/2023-03.txt', {encoding: 'utf8', flag: 'r'});
+        expect(getPartSum(input)).not.toBe(471097);
+        expect(getPartSum(input)).not.toBe(533735);
+        expect(getPartSum(input)).not.toBe(538039);
+        expect(getPartSum(input)).toBe(1);
+    });
+});
