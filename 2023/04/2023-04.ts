e6f0bbf7162fde29e6930e9cb76253114e877119
ivanburda1986 <burda.ivan@gmail.com>
2024-01-02 01:14:21 +0100

    2023-04-2 wip

--- 2023/04/2023-04.ts
+++ 2023/04/2023-04.ts
@@ -19,3 +19,28 @@ export const getCardWinningPoints = (input: string) => {
     });
     return total;
 };
+
+/*
+Create an object with card representations
+Create an array representing what cards the user has
+Iterate over the card array and for each:
+-evaluate winning numbers
+-push in numbers for next card copies depending on the winning number count
+-sort from lowest to highest
+
+Return card array length
+ */
+
+
+export const getCardCount = (input: string): number => {
+    const cards: Record<string, Record<string, string>> = {};
+
+    input.split('\n').map(card => {
+        const [cardName, allNumbers] = card.split(": ");
+        const [winningString, actualString] = allNumbers.split(" |");
+        const winning = winningString.split(' ').filter(Boolean);
+        const actual = actualString.split(' ').filter(Boolean);
+        cards[cardName] = {winning, actual};
+    });
+    return 0;
+};
--- 2023/04/__tests__/2023-04.test.ts
+++ 2023/04/__tests__/2023-04.test.ts
@@ -1,5 +1,5 @@
 import {readFileSync} from "fs";
-import {getCardWinningPoints} from "../2023-04";
+import {getCardCount, getCardWinningPoints} from "../2023-04";
 
 describe('AOC-2023-04', () => {
     it('returns 13 to be number of points scratchcards are worth for the test data', () => {
@@ -16,4 +16,21 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
         const input = readFileSync('2023/04/2023-04.txt', {encoding: 'utf8', flag: 'r'});
         expect(getCardWinningPoints(input)).toBe(26426);
     });
+
+    it('returns 30 to be number of scratchcards for the test data', () => {
+        const input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
+        expect(getCardCount(input)).toBe(30);
+    });
+
+    it('returns xx to be number of scratchcards for the test data', () => {
+        const input = readFileSync('2023/04/2023-04.txt', {encoding: 'utf8', flag: 'r'});
+        expect(getCardCount(input)).toBe(1);
+    });
+
+
 });
