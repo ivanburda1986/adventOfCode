dfe9f8db0636ab163bdf614c0180bbcf2a1b4850
ivanburda1986 <burda.ivan@gmail.com>
2024-01-14 14:22:53 +0100

    progress

--- 2023/04/2023-04.ts
+++ 2023/04/2023-04.ts
@@ -31,16 +31,59 @@ Iterate over the card array and for each:
 Return card array length
  */
 
+const getCardNumber = (cardName: string): string | undefined => {
+    const number = cardName.match(/\d+/);
+    if (!number) {
+        return undefined;
+    }
+    return number[0];
+};
+
+const getCardWinningNumberCount = (winning: string[], actual: string[]) => {
+    let points = 0;
+    for (let i = 0; i < actual.length; i++) {
+        if (winning.includes(actual[i])) {
+            points++;
+        }
+    }
+    console.log(points);
+    return points;
+};
+
+function createNumberObject(n: number) {
+    const resultObject: Record<string, number> = {};
+
+    for (let i = 1; i <= n; i++) {
+        resultObject[i] = 1;
+    }
+
+    return resultObject;
+}
+
 
 export const getCardCount = (input: string): number => {
-    const cards: Record<string, Record<string, string>> = {};
+    const cardRepresentations: Record<string, Record<string, string[]>> = {};
 
     input.split('\n').map(card => {
         const [cardName, allNumbers] = card.split(": ");
         const [winningString, actualString] = allNumbers.split(" |");
         const winning = winningString.split(' ').filter(Boolean);
         const actual = actualString.split(' ').filter(Boolean);
-        cards[cardName] = {winning, actual};
+        const number = getCardNumber(cardName);
+        if (number) {
+            cardRepresentations[number] = {winning, actual};
+        }
     });
+
+    const playerCardCount = createNumberObject(Object.entries(cardRepresentations).length);
+    for (let i = 1; i <= Object.entries(playerCardCount).length; i++) {
+        for (let j = 1; j <= playerCardCount[i.toString()]; j++) {
+            getCardWinningNumberCount(cardRepresentations[i.toString()].winning, cardRepresentations[i.toString()].actual);
+        }
+    }
+
+    // initialCardSet.map(card => getCardWinningNumberCount(card.winning, card.actual));
+
+
     return 0;
 };
--- 2023/04/__tests__/2023-04.test.ts
+++ 2023/04/__tests__/2023-04.test.ts
@@ -27,7 +27,7 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
         expect(getCardCount(input)).toBe(30);
     });
 
-    it('returns xx to be number of scratchcards for the test data', () => {
+    it('returns xx to be number of scratchcards for the real data', () => {
         const input = readFileSync('2023/04/2023-04.txt', {encoding: 'utf8', flag: 'r'});
         expect(getCardCount(input)).toBe(1);
     });
